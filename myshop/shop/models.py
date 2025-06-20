6423230569c3e4b3b3252f545fe5364b11f2a08d
Praval Sugandhi <pravalsugandhi@Pravals-MacBook-Air.local>
2025-06-20 15:40:21 -0400

    new commit for banner model only no connection

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 from django.contrib import admin
+
+#------------------------------------banner model------------------------------------
+class Banner(models.Model):
+    bannerid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='shop/media/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
 #------------------------------------user register model------------------------------------
 
 
