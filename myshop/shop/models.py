f8ad67f670746f79fd8b4f6fe50cc112287086c0
Praval Sugandhi <pravalsugandhi@Pravals-MacBook-Air.local>
2025-06-20 11:21:21 -0400

    login logout and register with reopen modal issue

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,12 +1,57 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+from django.contrib import admin
+#------------------------------------user register model------------------------------------
+
+
+
+class users(models.Model):
+    userid = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, default="")
+    last_name = models.CharField(max_length=30, default="")
+    username = models.CharField(max_length=50, unique=True, default="")
+    email = models.EmailField(unique=True, default="")
+    password = models.CharField(max_length=100, default="")  # Optional: store masked or leave blank
+    phone = models.CharField(max_length=15, null=True, blank=True, default="")
+
+    def __str__(self):
+        return self.username
+    
+
+
+
+
+#------------------------------------wishlist model------------------------------------
+    
+class wishlist(models.Model):
+    wishlistid = models.AutoField(primary_key=True)
+    userid = models.ForeignKey(users, on_delete=models.CASCADE)
+    productid = models.IntegerField()
+
+    def __str__(self):
+        return f"Wishlist {self.wishlistid} for User {self.userid.username}"
+
+
+
+
+#------------------------------------categories model------------------------------------
+
 class Categories(models.Model):
     categoryid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
+    
+
+
+
+
+
+
+#------------------------------------products model------------------------------------
 class Products(models.Model):
 
     productid = models.AutoField(primary_key=True)
@@ -21,3 +66,4 @@ class Products(models.Model):
     def __str__(self):
         return f"{self.productid} - {self.name}"
 
+    
--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -3,7 +3,10 @@ from shop.views import *
 
 urlpatterns = [
     path('',home, name='home'),
+    path('register/', register, name='register'),
+    path('login/', userlogin, name='login'),
     path('product/<int:myproductid>/',productdetail, name='productdetail'),
     path('wishlist/', wishlist, name='wishlist'),
-    
+    path('checkout/', checkout, name='checkout'),
+    path('logout/', logoutuser, name='logout'),   
 ]
